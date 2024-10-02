4d292a9e5115a8b56e24e72a1f3fa0acfb726efe
H William <willskhughes.edu@gmail.com>
2024-10-01 21:20:39 -0700

    Add: 1-square.py file.

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Define a Square Class"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initialize the square class with a private size attribute."""
+        self.__size = size
