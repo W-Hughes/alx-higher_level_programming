b5f01f5eccb4504b77470adb7c8acc847d84c94a
W Hughes <snypgeee@gmail.com>
2024-10-21 22:26:57 -0700

    feat/Update: Updated the Rectangle instance with area.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -61,3 +61,7 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """Defines the area of the Rectangle Instance."""
+        return self.width * self.height
