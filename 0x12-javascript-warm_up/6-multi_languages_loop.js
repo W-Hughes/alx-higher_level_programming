0173b22d7457f4f373ebdd35c5d61220f3e4b486
W Hughes <snypgeee@gmail.com>
2025-01-26 16:12:13 -0800

    Add: 6-multi_languages_loop.js file.

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// This script prints 3 lines of strings using an array of strings and a loop.
+const texts = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const text of texts) {
+  console.log(text);
+}
